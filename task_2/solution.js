f218f7e579c6deaaaab482b844c70d19b264085f
svg0191 <svg0191@gmail.com>
2023-02-14 00:08:37 +0500

    Вариант решения 7

--- task_2/solution.js
+++ task_2/solution.js
@@ -5,13 +5,13 @@ function calcShipping(sum, min, shipping) {
 
     // Задание №2.1. Рассчитать доставку
 
-    
      let shippingSum; //создайте переменную shippingSum
 
      if ((productsSum == 0) || (productsSum >= freeShippingMinSum)) {
          shippingSum = 0;
-     } else {((productsSum > 0) && (productsSum < freeShippingMinSum))
-
+     } else {((productsSum > 0) && (productsSum < freeShippingMinSum)) {
+        shippingSum = shippingPrice
+     }
      }
 
     // Конец решения задания №2.1.
