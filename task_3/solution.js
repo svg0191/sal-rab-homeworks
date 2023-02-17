aff5bfe9b7933c6764bf07842b79b708de604c9d
svg0191 <svg0191@gmail.com>
2023-02-18 00:45:43 +0500

    Вариант решения задачи 33

--- task_3/solution.js
+++ task_3/solution.js
@@ -2,7 +2,6 @@ function renderCartItem(item) {
     const product = item;
 
     // Задание №3.1. Формирование строки корзины
-
     
     // например, {id: 1, title: 'Пицца', count: 5, price: '500.00'}, где
     // id - идентификатор
@@ -19,7 +18,7 @@ function renderCartItem(item) {
     itemCountText += " × "; // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
     itemCountText += product.price; // 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
     itemCountText += " ₽ = "; // 5. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽ = `
-    let sum = product.count × product.price; // 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
+    let sum = product.count * product.price; // 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
     itemCountText += sum; // 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
     itemCountText += " ₽"; // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
 
