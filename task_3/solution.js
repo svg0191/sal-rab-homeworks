1bcc1717b4e1934cc0028e0c5967b9e9ebdc9128
svg0191 <svg0191@gmail.com>
2023-02-17 23:38:44 +0500

    Вариант решения задачи 28

--- task_3/solution.js
+++ task_3/solution.js
@@ -16,12 +16,12 @@ function renderCartItem(item) {
     // 5 * 500.00 ₽ = 2500 ₽
     let itemCountText = " "; // 1. Создайте переменную itemCountText, равную пустой строке
     itemCountText += product.count; // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
-    itemCountText +=   "*" ; // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` * `
+    itemCountText +=   " * " ; // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` * `
     itemCountText += product.price; // 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
-    itemCountText += ` ₽ = `; // 5. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽ = `
+    itemCountText += " ₽ = "; // 5. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽ = `
     let sum = product.count * product.price; // 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
     itemCountText += sum; // 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
-    itemCountText += ` ₽`; // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
+    itemCountText += " ₽"; // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
 
     // Конец решения задания №3.1.
 
