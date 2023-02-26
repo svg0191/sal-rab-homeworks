164db2e0cc52c276dd3956fbdc12a49c43b6206d
svg0191 <svg0191@gmail.com>
2023-02-26 19:37:37 +0500

    Попытка №6

--- task_final/solution.js
+++ task_final/solution.js
@@ -43,9 +43,9 @@ function sendRequest(name, phone, address, goods, sum) {
       count: goods[i].count
     });
   }
-  
+
   data.client = `${name} ${phone}`;
-  data.order.address = `ул. ${address.street}, дом  ${address.house}, ${address.entrance} подъезд, ${address.floor}  этаж, кв.  ${address.flat}`;
+  data.order.address = `ул. ${address.street}, дом  ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв. ${address.flat}`;
   data.order.sum = sum;
   let jsonData = JSON.stringify({
     data
