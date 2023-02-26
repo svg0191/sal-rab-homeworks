8b9bfdc39a92a597e3adae07b347cdd63dd2ab10
svg0191 <svg0191@gmail.com>
2023-02-26 18:32:11 +0500

    JSON из комментариев

--- task_final/solution.js
+++ task_final/solution.js
@@ -55,7 +55,7 @@ function sendRequest(name, phone, address, goods, sum) {
 
 
         
-    let jsonData = JSON.stringify({data});
+    let jsonData = JSON.stringify({data: data});
 
     return jsonData;
 }
