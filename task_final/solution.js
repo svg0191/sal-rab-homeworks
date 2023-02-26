f5d490eaa15b1eb97abf4868a7f22ae9bcf6f674
svg0191 <svg0191@gmail.com>
2023-02-26 18:57:13 +0500

    Взято из консоли

--- task_final/solution.js
+++ task_final/solution.js
@@ -37,22 +37,18 @@ function sendRequest(name, phone, address, goods, sum) {
     let data = {client: {name, phone}, order: {address, sum}, goods: []};
 
     let countOfGoods = goods.length;
-
-    for (let i = 0; i < countOfGoods; i += 1) {
-        data.goods.push({title: goods[i].title, count: goods[i].count});
-    }
-
-    data.client = `${name} ` + `${phone}`;
-    data.order.address = address;
-    address = `'ул. ' + ${address.street}, 'дом ' + ${address.house}, ${address.entrance} + ' подъезд', ${address.floor} + ' этаж', 'кв ' + ${address.flat}`;
-    data.order.sum = sum;
-
-
-    
-
-
-        
-    let jsonData = JSON.stringify({data});
-
-    return jsonData;
+  for (let i = 0; i < countOfGoods; i += 1) {
+    data.goods.push({
+      title: goods[i].title,
+      count: goods[i].count
+    });
+  }
+  data.client = `${ name } ` + `${ phone }`;
+  data.order.address = address;
+  address = `'ул. ' + ${ address.street }, 'дом ' + ${ address.house }, ${ address.entrance } + ' подъезд', ${ address.floor } + ' этаж', 'кв ' + ${ address.flat }`;
+  data.order.sum = sum;
+  let jsonData = JSON.stringify({
+    data
+  });
+  return jsonData;
 }
