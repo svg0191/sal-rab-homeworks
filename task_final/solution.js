cc1579e3e3dc62ceaeb7126246c56b39e3ea0808
svg0191 <svg0191@gmail.com>
2023-02-26 18:45:36 +0500

    Исправлена строка адреса

--- task_final/solution.js
+++ task_final/solution.js
@@ -42,9 +42,10 @@ function sendRequest(name, phone, address, goods, sum) {
         data.goods.push({title: goods[i].title, count: goods[i].count});
     }
 
-    data.client = `${name} ` + `${phone}`;
+    data.client = `${name} ``${phone}`;
     data.order.address = address;
-    address = `'ул. ' + ${address.street}, 'дом ' + ${address.house}, ${address.entrance} + ' подъезд', ${address.floor} + ' этаж', 'кв ' + ${address.flat}`;
+    address = "ул. " + address.street + ", дом " + address.house + " , " + address.entrance + " подъезд," + address.floor + " этаж, " + "кв " + address.flat;
+    
     data.order.sum = sum;
 
     
