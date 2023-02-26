f4351a44d40d437c47b12fa6499470a0e404593f
svg0191 <svg0191@gmail.com>
2023-02-26 18:35:57 +0500

    В data аргументы сделала в строку

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,11 +34,7 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {
-        client: {name, phone}, 
-        order: {address, sum}, 
-        goods: []
-    };
+    let data = {client: {name, phone}, order: {address, sum}, goods: []};
 
     let countOfGoods = goods.length;
 
